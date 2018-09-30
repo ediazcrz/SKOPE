f8adc5af3d07cece43a4bd1241370255f6dbe3f1
Carlo Palacios <chorri_33@Hotmail.com>
2018-09-30 03:34:20 -0400

    added gps

---
+++ SeniorProject - ARv3/Assets/GPS.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPS : MonoBehaviour
+{
+
+    public static GPS Instance { set; get; }            // Makes it a property
+
+    public float latitude;
+    public float longitude;
+    //public float altitude;
+
+    // Use this for initialization
+    private void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);                  // Just in case you change scenes
+        StartCoroutine(StartLocationService());         // Send request for your phone to use GPS //Parallel action
+    }
+
+    private IEnumerator StartLocationService()          // Colections
+    {
+        while (true)
+        {
+            if (!Input.location.isEnabledByUser)
+            {
+                Debug.Log("User has not enabled GPS");
+                yield break;                                // ends the coroutine                          
+            }
+
+            Input.location.Start(1f, .1f);
+
+            int maxWait = 20;
+
+            while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+            {
+                yield return new WaitForSeconds(1);            // while loop will be called 20 times
+                maxWait--;
+            }
+
+            if (maxWait <= 0)
+            {
+                Debug.Log("Timed out");
+                yield break;
+            }
+
+            if (Input.location.status == LocationServiceStatus.Failed)
+            {
+                Debug.Log("Unable to determine device location");
+                yield break;
+            }
+
+            latitude = Input.location.lastData.latitude;
+            longitude = Input.location.lastData.longitude;
+            //altitude = Input.location.lastData.altitude;
+
+            //yield break;
+        }
+        //Input.location.Stop();
+    }
+}
+
---
+++ SeniorProject - ARv3/Assets/UpdateGPSText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateGPSText : MonoBehaviour
+{
+
+    public Text coordinates;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        coordinates.text = "Latitude:" + GPS.Instance.latitude.ToString() +
+            "   \nLongitude:" + GPS.Instance.longitude.ToString();// +
+            //"   \nAltitude:" + GPS.Instance.altitude.ToString();
+    }
+}
